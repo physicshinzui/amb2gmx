f512c9f8047215f63f37a3460952a7f21a77d6f4
siida <physicshinzui@gmail.com>
2022-05-26 18:17:26 +0900

    Delare GMX instead of using gmx

--- 01-build.sh
+++ 01-build.sh
@@ -3,9 +3,10 @@ set -eu
 
 PDB=$1
 FF="ff14SB"
+GMX=gmx
 
 # === (0) Preprocessing: 
-gmx editconf -center 0 0 0 -f ${PDB} -o $(basename ${PDB})
+${GMX} editconf -center 0 0 0 -f ${PDB} -o $(basename ${PDB})
 
 # === (1) pdb4amber: 
 pdb4amber -i $(basename $PDB) -o pdb4amber.pdb
@@ -37,8 +38,8 @@ cp -p system.amb2gmx/system_GMX.top system.top
 
 echo '"Protein" | "Ion"' > inp
 echo quit >> inp
-gmx make_ndx -f system.gro -o index_ProeinIon.ndx < inp
-echo Protein_Ion | gmx trjconv -f system.gro -s system.gro -o system.gro -n index_ProeinIon.ndx
+${GMX} make_ndx -f system.gro -o index_ProeinIon.ndx < inp
+echo Protein_Ion | ${GMX} trjconv -f system.gro -s system.gro -o system.gro -n index_ProeinIon.ndx
 
 cat system.top   | \
         sed '$d' | \
@@ -49,7 +50,7 @@ rm inp index_ProeinIon.ndx
 
 # === (5) Generate position restraint for main chain
 posres_itp=posre_mainchain.itp
-echo MainChain | gmx genrestr -f system.gro -o ${posres_itp}
+echo MainChain | ${GMX} genrestr -f system.gro -o ${posres_itp}
 
 # === (6) Insert ifdef for position restraints.
 iloc=$(nl -b a system.top | grep "moleculetype" | awk 'NR==2 {print $1}' | bc)
@@ -58,14 +59,14 @@ gsed -i "${iloc} i #ifdef POSRES\n#include \"${posres_itp}\"\n#endif\n" system.t
 # This is GNU sed installed by Homebrew on mac. If you are in a linux, replace gsed with sed
 
 # === (7) Build a system by gmx.
-echo Protein | gmx editconf -f system.gro -o box.gro -princ -bt cubic -d 1.5
+echo Protein | ${GMX} editconf -f system.gro -o box.gro -princ -bt cubic -d 1.5
 
 ## === (7.1) insert molecule for cosolvent MD: 
 nmol=10
-gmx insert-molecules -f box.gro -ci ligand.amb2gmx/ligand_GMX.gro -o box_inserted.gro -nmol $nmol -scale 3
+${GMX} insert-molecules -f box.gro -ci ligand.amb2gmx/ligand_GMX.gro -o box_inserted.gro -nmol $nmol -scale 3
 
 ## === (7.2) Solvate
-gmx solvate -cp box_inserted.gro -cs spc216.gro -o mol_solv.gro -p system.top
+${GMX} solvate -cp box_inserted.gro -cs spc216.gro -o mol_solv.gro -p system.top
 
 ## === (7.3) Merge topologies: protein and ligand
 # TODO: This part is hard coded and mannually done. I want to automate here.  
@@ -84,8 +85,8 @@ mv modified.top system.top
 
 ## ==(7.4) Add ions
 read -p "How many warnings are allowd?" nwarns
-gmx grompp -f templates/ions.mdp -c mol_solv.gro -p system.top -o ions.tpr -maxwarn $nwarns
-echo SOL | gmx genion -s ions.tpr -o mol_solv_ions.gro -p system.top -pname NA+ -nname CL- -neutral
+${GMX} grompp -f templates/ions.mdp -c mol_solv.gro -p system.top -o ions.tpr -maxwarn $nwarns
+echo SOL | ${GMX} genion -s ions.tpr -o mol_solv_ions.gro -p system.top -pname NA+ -nname CL- -neutral
 
 bash trash.sh
 
